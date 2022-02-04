c6f4a226e90b3ac3fcb2bfccbd6399095f1d14f9
Ken <ken@vaczoway.com>
2022-02-04 10:26:51 -0600

    Initial Commit

---
+++ wordgameart/wordgameartApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordgameartApp.swift
+//  wordgameart
+//
+//  Created by Ken on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct wordgameartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
